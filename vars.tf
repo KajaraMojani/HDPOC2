1a76c390992a148a75c382131c24087beaba9282
KajaraMojani <105744543+KajaraMojani@users.noreply.github.com>
2022-05-27 22:37:57 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,12 +2,29 @@ variable "databricks_account_username" {}
 variable "databricks_account_password" {}
 variable "databricks_account_id" {}
 
-variable "tags" {
-  default = {}
+variable "databricks_account_name" {
+  type = string
 }
 
-variable "cidr_block" {
-  default = "194.160.0.0/16"
+variable "required_az_total" {
+  default = "3"
+}
+
+variable "cidr_block_host" {
+  default = "192.160.0.0"
+}
+
+variable "cidr_block_prefix" {
+  default = "16"
+}
+
+variable "subnet_offset" {
+  default = 3
+}
+
+
+variable "tags" {
+  default = {}
 }
 
 variable "region" {
@@ -15,5 +32,17 @@ variable "region" {
 }
 
 locals {
-  prefix = "nhs-sba-test2"
+  prefix = "nhs-sba-test4"
+  cidr_block = "${var.cidr_block_host}/${var.cidr_block_prefix}"
+  
+  small_subnet_cidrs = [for i in range(0, 10) : cidrsubnet(cidrsubnet(local.cidr_block, var.subnet_offset, pow(2, var.subnet_offset)-1),
+   32 - var.cidr_block_prefix - var.subnet_offset - 4,
+    pow(2, 32 - var.cidr_block_prefix - var.subnet_offset - 4) - 1 - i)]
+
+  required_azs = var.required_az_total == "all" ? length(data.aws_availability_zones.available.names) : tonumber(var.required_az_total)
+}
+
+variable "aws-iam-arn" {
+default="arn:aws:iam::303037622152:role/service/nhs-dmsmdm-sba-flood-iam-role"
+
 }
